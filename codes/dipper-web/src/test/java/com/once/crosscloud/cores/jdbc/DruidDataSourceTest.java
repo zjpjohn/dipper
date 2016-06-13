f0a39991cb6478dd01cee797cfc64b56c6668454
netgenius <wuheng09@otcaix.iscas.ac.cn>
2016-06-13 21:08:04 +0800

    add DruidDataSourceTest

--- codes/dipper-web/src/test/java/com/once/crosscloud/cores/jdbc/DruidDataSourceTest.java
+++ codes/dipper-web/src/test/java/com/once/crosscloud/cores/jdbc/DruidDataSourceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.once.crosscloud.jdbc;
+package com.once.crosscloud.cores.jdbc;
 
 import com.alibaba.druid.filter.config.ConfigTools;
 
